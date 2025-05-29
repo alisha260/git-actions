644ca5a9cb02d4a6bddd11c62996ea54a1c102bf
alisha260 <meharalisha45@gmail.com>
2025-05-29 15:07:44 +0500

    Create g.py

---
+++ g.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+# Input: get two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition
+sum = num1 + num2
+
+# Output: print the result
+print("The sum is:", sum)
